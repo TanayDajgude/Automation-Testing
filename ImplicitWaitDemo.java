0b674da39e6caacc75b185621b5977eb48d3e9d7
Tanay Dajgude <115832137+TanayDajgude@users.noreply.github.com>
2024-02-06 00:14:13 +0530

    Add files via upload

---
+++ ImplicitWaitDemo.java
@@ -0,0 +1,28 @@
+package day6;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ImplicitWaitDemo {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //implicit wait command
+		// The implicit wait command wait for specified time to load the element and if found before time then it goes to next element.
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+
+
+	}
+
+}
